3c1a0a81ed7bd53d5ce2831366db52f6070a6f33
bonnieho <bonnieho@rice.edu>
2019-04-29 23:52:09 -0500

    started jQuery media queries in app.js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -22,4 +22,14 @@ $(window).on('changed.zf.mediaquery', function(event, newSize, oldSize){
 	if(newSize ==='small' && oldSize === 'medium') {
 		$('main-menu ul').removeClass('expanded');
 	}
+
+	if(newSize ==='medium' && oldSize === 'small') {
+		$('main-menu ul').addClass('expanded');
+	}
 });
+
+var size = Foundation.MediaQuery.current;
+//console.log(size);
+if(size === 'small'){
+	$('main-menu ul').removeClass('expanded');
+}
